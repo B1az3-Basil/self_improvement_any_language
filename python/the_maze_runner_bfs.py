8d230b4d9f41efc04895a2c2f519f1cf7d743216
Basil-B1az3 <basil-b1az3@debian.Basil-b1az3>
2021-02-06 15:44:38 +0200

    tying out the mazerunner

--- python/the_maze_runner_bfs.py
+++ python/the_maze_runner_bfs.py
@@ -1,7 +1,9 @@
+import time
 import battle_maze
 import turtle 
 
 obstacles = battle_maze.get_obstacles()
+# obstacles = []
 
 def draw_turtle():
     turtle.pencolor("red")
@@ -38,64 +40,67 @@ step = 5
     
 def bfs_solve():
 
-    space = []
+    space = [(x,y) for x in range(-100, 101, step) for y in range(-200, 201, step) if not battle_maze.is_position_blocked(x,y)]
+    # print(space)
     x, y = -100, 200
-    while x != 101:
-        y = 200
-        while y != -201:
-            if not battle_maze.is_position_blocked(x, y):
-                space.append((x, y))
-            y -= 1
-        x += 1
-
+    
+    # print((0, 7) in space)
     draw_turtle()
     position = [{1:(0,0)}]
     star_coor = [(0,0)]
     x, y = 0, 0
     to_get = (0, 0)
-    turtle.speed(1)
+    turtle.delay(1)
     turtle.penup()
     check = True
     index = 2
     for x in position:
+        # print(x)
         index = list(x.keys())[0] + 1
         x = list(x.values())[0]
-
+        # star_coor.append(x)
+        # print(star_coor)
         if (x[0] + step, x[1]) in space and not (x[0] + step, x[1]) in star_coor:
             turtle.goto(x[0] + step, x[1])
-            star_coor.append((x[0] + step,x[1]))
+            star_coor.append((x[0] + step, x[1]))
             position.append({index:(x[0] + step, x[1])})
             turtle.stamp()
+            print("right", (x[0] + step, x[1]))
 
         if (x[0] - step, x[1]) in space and not (x[0] - step, x[1]) in star_coor:
             turtle.goto(x[0] - step, x[1])
             star_coor.append((x[0] - step, x[1]))
             position.append({index: (x[0] - step, x[1])})
             turtle.stamp()
+            print('left', (x[0] - step, x[1]))
 
         if (x[0], x[1] + step) in space and not (x[0], x[1] + step) in star_coor:
             turtle.goto(x[0], x[1] + step)
             star_coor.append((x[0], x[1] + step))
             position.append({index: (x[0], x[1] + step)})
             turtle.stamp()
+            print("up", (x[0], x[1] + step))
         
         if (x[0], x[1] - step) in space and not (x[0], x[1] - step) in star_coor:
             turtle.goto(x[0], x[1] - step)
             star_coor.append((x[0], x[1] - step))
-
             position.append({index : (x[0], x[1] - step)})
             turtle.stamp()
+            print("down", (x[0], x[1] + step))
         
         if x[1] + step == 200:
             to_get = (x[0], 200)
             break
-    turtle.tracer(1)
+
+        # if index == 2:
+        #     break
+    
     the_path = []   
     print("found")
     turtle.pendown()
     index -= 1
     turtle.pencolor("green")
-    while index != 1:
+    while index != 0:
         if {index: (to_get[0], to_get[1] - step)} in position:
             to_get = (to_get[0], to_get[1] - step)
             turtle.goto(to_get)
