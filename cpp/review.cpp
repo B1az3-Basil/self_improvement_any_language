fcd07a7dfe5335a383adcd1568b177b55c174d88
b1az3-basil <basilmokgadi1@gmail.com>
2021-02-13 21:47:15 +0200

    added the print statement which prints out the hdds

--- cpp/review.cpp
+++ cpp/review.cpp
@@ -19,7 +19,15 @@ class person{
             location = "";
             email_address = "";
         }
+        person(string date, string name, string ussd, string location, string email){
+            this->date = date;
+            this->name = name;
+            this->ussd = ussd;
+            this->location = location;
+            this->email_address = email; 
+        }
 };
+
 
 vector<string> split(char to_split ,string word){
     vector<string> list; 
@@ -36,23 +44,37 @@ vector<string> split(char to_split ,string word){
 }
 
 
-
 vector<string> run_command(string command){
     char buffer[128];
     string result;
     FILE *pipe = popen(command.c_str(),"r");
-    // if (!pipe) return  vector<string>;
-    // cout << fgets(buffer, 128, pipe) << endl;
     while (!feof(pipe)) if (fgets(buffer, 128, pipe) != NULL)  result += buffer;
-    // printf("%s", result.c_str());
     pclose(pipe);
     return split('\n',result);
 }
 
+
+bool in(string to_find, auto from){
+    int i = 0;
+    for(auto a: from){
+        if (i == to_find.size() - 1) return true;
+        else if (to_find[i] == a) i++;
+        else i = 0;
+    } 
+    return false;
+}
+
+
 int main(){
     // int command = system("wtc-lms");
     vector<string> all_review_list = run_command("wtc-lms reviews");
-    for (string a: all_review_list) cout << a.find("Assigned-") << endl;
-    printf("%s", all_review_list[0].c_str());
+    for (string a: all_review_list) {
+        if (in("Assigned", a)){
+            printf("%s\n", a.c_str());
+            printf("%s\n", a.substr(a.find('(') + 1, a.find(')') - a.find('(') - 1).c_str());
+            break;
+        }
+    }
+    // printf("%s", all_review_list[0].c_str());
     return 0;
 }
