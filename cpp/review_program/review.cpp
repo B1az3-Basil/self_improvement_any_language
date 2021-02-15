2b423a5fae977ce84dcfe7f8029eee4a664068f8
b1az3-basil <basilmokgadi1@gmail.com>
2021-02-15 16:10:25 +0200

    killed some bugs

--- cpp/review_program/review.cpp
+++ cpp/review_program/review.cpp
@@ -1,7 +1,5 @@
+#include "person_data.h"
 #include "write_things_in_binaryFile.cpp"
-
-using namespace std;
-
 
 vector<string> split(char to_split ,string word){
     vector<string> list; 
@@ -38,16 +36,49 @@ bool in(string to_find, auto from){
     return false;
 }
 
+string get_date(){
+   // current date/time based on current system
+    time_t now = time(0);
+    tm *ltm = localtime(&now);
+    string day = to_string(1900 + ltm->tm_year) + '-' + to_string(1 + ltm->tm_mon) + '-' + to_string(7 + ltm->tm_mday);
+    return day;
+
+}
+
 
 int main(){
     // int command = system("wtc-lms");
     vector<string> all_review_list = run_command("wtc-lms reviews");
+    vector<vector<string>> hssd;
+    vector<string> hash;
     for (string a: all_review_list) {
         if (in("Assigned", a)){
-            printf("%s\n", a.substr(a.find('(') + 1, a.find(')') - a.find('(') - 1).c_str());
+            hash.push_back(a.substr(a.find('(') + 1, a.find(')') - a.find('(') - 1));
+            hssd.push_back(run_command("wtc-lms review_details " + a.substr(a.find('(') + 1, a.find(')') - a.find('(') - 1)));
         }
     }
-    write_file the_file();
+    printf("%s\n", get_date().c_str());
+    int w = 0;
+    vector<person> ins;
+    for (auto a: hssd){
+        string loca;
+        string email_name;
+        for (auto z: a) {
+            if (in("@student", z)) email_name = z.substr(z.find(':') + 2, 100);
+            else if (in("Location", z)) loca = z.substr(z.find(' ') + 1, 100);
+        }
+
+        printf("hash: %s\n", hash[w].c_str());
+        printf("loca: %s\n", loca.c_str());
+        printf("email: %s\n", email_name.c_str());
+        printf("name: %s\n", email_name.substr(0,  email_name.find('@')).c_str());
+        person one;
+        one.persons(get_date(), email_name.substr(0,  email_name.find('@')), hash[w], loca,email_name);
+        ins.push_back(one);
+        w++;
+    }
+    
+    write_file_in(ins);
     // printf("%s", all_review_list[0].c_str());
     return 0;
 }
--- cpp/review_program/write_things_in_binaryFile.cpp
+++ cpp/review_program/write_things_in_binaryFile.cpp
@@ -1,26 +1,25 @@
+#include "person_data.h"
 
-#include "person_data.hpp"
-// #include "review.cpp"
-using namespace std; 
+void write_file_in(vector<person> temp){
+    fstream file("data.bin",ios::binary | ios::in | ios::out | ios::trunc);
+    if(!file.is_open()){
+        cout << "error while opening the file";
+        return;
+    }
 
-class write_file{
-    private:
-        vector<person> the_data;
-        fstream file;
-    public:
-        void write_fil(){
-            fstream file("data.bin",ios::binary | ios::in | ios::out | ios::trunc);
-        }
+    for(auto y: temp) {
+        file.write((char *)&y,sizeof(person));
+    }
+    file.seekg(0);
+    file.close();
+}
 
-        void write_fie(vector<person> temp){
-            for(person& y: temp) file.write((char *)&y,sizeof(person));
-            file.close();
-        }
-        
-        vector<person> read_file(){
-            person temp;
-            while(file.read((char *)&temp,sizeof(person))) the_data.push_back(temp);
-            file.close();
-            return the_data;
-        } 
-};
+vector<person> read_file(){
+    fstream file("data.bin",ios::binary | ios::in | ios::out | ios::trunc);
+    vector<person> the_data;
+    person temp;
+    while(file.read((char *)&temp,sizeof(person))) the_data.push_back(temp);
+    
+    file.close();
+    return the_data;
+} 
