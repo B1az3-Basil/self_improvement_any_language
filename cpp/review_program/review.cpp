4ad5d701afbd05006c2271c94939fd13d652d9ab
b1az3-basil <basilmokgadi1@gmail.com>
2021-02-16 17:54:27 +0200

    removed the binary functions and commented some other things to run the comments

--- cpp/review_program/review.cpp
+++ cpp/review_program/review.cpp
@@ -1,6 +1,7 @@
 #include "person_data.h"
 #include "write_things_in_binaryFile.cpp"
 
+
 vector<string> split(char to_split ,string word){
     vector<string> list; 
     string temp;
@@ -42,43 +43,37 @@ string get_date(){
     tm *ltm = localtime(&now);
     string day = to_string(1900 + ltm->tm_year) + '-' + to_string(1 + ltm->tm_mon) + '-' + to_string(7 + ltm->tm_mday);
     return day;
-
 }
+
 
 
 int main(){
-    // int command = system("wtc-lms");
-    vector<string> all_review_list = run_command("wtc-lms reviews");
-    vector<vector<string>> hssd;
-    vector<string> hash;
-    for (string a: all_review_list) {
-        if (in("Assigned", a)){
-            hash.push_back(a.substr(a.find('(') + 1, a.find(')') - a.find('(') - 1));
-            hssd.push_back(run_command("wtc-lms review_details " + a.substr(a.find('(') + 1, a.find(')') - a.find('(') - 1)));
-        }
-    }
-    printf("%s\n", get_date().c_str());
-    int w = 0;
-    vector<person> ins;
-    for (auto a: hssd){
-        string loca;
-        string email_name;
-        for (auto z: a) {
-            if (in("@student", z)) email_name = z.substr(z.find(':') + 2, 100);
-            else if (in("Location", z)) loca = z.substr(z.find(' ') + 1, 100);
-        }
+    // vector<string> all_review_list = run_command("wtc-lms reviews");
+    // vector<vector<string>> hssd;
+    vector<string> the_list;
+    // for (string a: all_review_list) {
+    //     if (in("Assigned", a)){
+    //         hash.push_back(a.substr(a.find('(') + 1, a.find(')') - a.find('(') - 1));
+    //         hssd.push_back(run_command("wtc-lms review_details " + a.substr(a.find('(') + 1, a.find(')') - a.find('(') - 1)));
+    //     }
+    // }
+    // printf("%s\n", get_date().c_str());
+    // int w = 0;
+    // vector<person> ins;
+    // for (auto a: hssd){
+    //     string loca;
+    //     string email_name;
+    //     for (auto z: a) {
+    //         if (in("@student", z)) email_name = z.substr(z.find(':') + 2, 100);
+    //         else if (in("Location", z)) loca = z.substr(z.find(' ') + 1, 100);
+    //     }
 
-        printf("hash: %s\n", hash[w].c_str());
-        printf("loca: %s\n", loca.c_str());
-        printf("email: %s\n", email_name.c_str());
-        printf("name: %s\n", email_name.substr(0,  email_name.find('@')).c_str());
-        person one;
-        one.persons(get_date(), email_name.substr(0,  email_name.find('@')), hash[w], loca,email_name);
-        ins.push_back(one);
-        w++;
-    }
-    
-    write_file_in(ins);
-    // printf("%s", all_review_list[0].c_str());
+    //     person one(get_date(), email_name.substr(0,  email_name.find('@')), hash[w], loca,email_name);
+    //     ins.push_back(one);
+    //     w++;
+    // }
+    // write_file_in(ins);
+    the_list = read_file();
     return 0;
 }
+
