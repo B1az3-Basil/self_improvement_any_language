b61d477a9442374754a0fee34c487e58a5caae79
b1az3-basil <basilmokgadi1@gmail.com>
2021-02-16 17:55:13 +0200

    added new functing to write to the text filr

--- cpp/review_program/write_things_in_binaryFile.cpp
+++ cpp/review_program/write_things_in_binaryFile.cpp
@@ -1,25 +1,40 @@
 #include "person_data.h"
 
-void write_file_in(vector<person> temp){
-    fstream file("data.bin",ios::binary | ios::in | ios::out | ios::trunc);
-    if(!file.is_open()){
-        cout << "error while opening the file";
-        return;
-    }
-
-    for(auto y: temp) {
-        file.write((char *)&y,sizeof(person));
-    }
-    file.seekg(0);
-    file.close();
-}
 
 vector<person> read_file(){
-    fstream file("data.bin",ios::binary | ios::in | ios::out | ios::trunc);
-    vector<person> the_data;
-    person temp;
-    while(file.read((char *)&temp,sizeof(person))) the_data.push_back(temp);
+    vector <person> ofter;
+    string myText;
+    string array[5];
+    ifstream MyReadFile("data.txt");
+    getline (MyReadFile, myText);
+    while (myText == "*") {
+       getline (MyReadFile, array[0]);
+       getline (MyReadFile, array[1]);
+       getline (MyReadFile, array[2]);
+       getline (MyReadFile, array[3]);
+       getline (MyReadFile, array[4]);
+       getline (MyReadFile, myText);
+       person one(array[2], array[0], array[3], array[4], array[1]);
+       ofter.push_back(one);
+    }
     
-    file.close();
-    return the_data;
+    MyReadFile.close();
+    return ofter;
 } 
+
+
+void write_file_in(vector<person> temp){
+    ofstream myfile;
+    myfile.open ("data.txt");
+    for (auto a: temp){
+        myfile << "*\n";
+        myfile << a.name + "\n";
+        myfile << a.email_address + "\n";
+        myfile << a.date + "\n";
+        myfile << a.ussd + "\n";
+        myfile << a.location + "\n";
+       
+    }
+    myfile.close();
+  
+}
